7d32922673cad827bfa6f8ce8a5ae270a401fadb
amirhossein-mohammadpoor <mohammadpoor104@gmail.com>
2020-09-02 18:06:54 +0430

    ADD Router to APP.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from "react"
+import React, { Suspense } from 'react'
 import "./App.scss"
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import ToDoList from "./containers/ToDoList"
+import { ThemeProvider } from "@material-ui/core/styles"
+import CircularProgress from '@material-ui/core/CircularProgress'
+import theme from "./themes/theme"
 
-const App = () => {
+const App = props => {
   return (
-    <div></div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ToDoList} />
+          <Route exact path="/create" render={() => {
+            return (
+              
+            )
+          }} />
+          <Route exact path="/edit/:id" render={() => {
+            return (
+              
+            )
+          }} />
+          <Route render={() => <h1>Not Found</h1>} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   )
 }
 
